737682b533a5d7f9542438f5e8d1dc20ef4dbff2
Charlie Harte <charlieharte@Charlies-MBP.attlocal.net>
2023-01-11 23:17:05 -0500

    first

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for(let x=0;x<weeklyExpenseQuestions.length;i++){
+    let answer=parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses=weeklyExpenses+answer
+}
+for(let x=0;x<monthlyExpenseQuestions.length;i++){
+    let answer=parseFloat(window.prompt(monthlyExpenseQuestions[0]))
+monthlyExpenses=monthlyExpenses+answer
+}
