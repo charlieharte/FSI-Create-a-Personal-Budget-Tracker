3b5f8d14447b1aebbed15ce48b308ebca24c0af5
Charlie Harte <charlieharte@Charlies-MBP.attlocal.net>
2023-01-11 23:22:30 -0500

    second push

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-for(let x=0;x<weeklyExpenseQuestions.length;i++){
-    let answer=parseFloat(window.prompt(weeklyExpenseQuestions[0]))
-weeklyExpenses=weeklyExpenses+answer
-}
+let answer = parseFloat (window.prompt(weeklyExpenseQuestions[0]))
+console.log(answer)
